d982deabb8386ee0c6b8fd0bc82e345a85d94442
ulfsv <ulf.e.sv@gmail.com>
2020-10-14 16:31:05 +0200

    ParkedViewModel added

---
+++ Garage_2/Migrations/20201014135735_New.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Garage_2.Migrations
+{
+    public partial class New : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "ParkedVehicle",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ParkedDateTime",
+                value: new DateTime(2020, 10, 14, 13, 55, 34, 428, DateTimeKind.Local).AddTicks(5535));
+
+            migrationBuilder.UpdateData(
+                table: "ParkedVehicle",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ParkedDateTime",
+                value: new DateTime(2020, 10, 14, 15, 57, 34, 438, DateTimeKind.Local).AddTicks(5763));
+
+            migrationBuilder.UpdateData(
+                table: "ParkedVehicle",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ParkedDateTime",
+                value: new DateTime(2020, 10, 14, 15, 54, 34, 438, DateTimeKind.Local).AddTicks(5975));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "ParkedVehicle",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ParkedDateTime",
+                value: new DateTime(2020, 10, 14, 13, 36, 52, 505, DateTimeKind.Local).AddTicks(817));
+
+            migrationBuilder.UpdateData(
+                table: "ParkedVehicle",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ParkedDateTime",
+                value: new DateTime(2020, 10, 14, 15, 38, 52, 513, DateTimeKind.Local).AddTicks(112));
+
+            migrationBuilder.UpdateData(
+                table: "ParkedVehicle",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ParkedDateTime",
+                value: new DateTime(2020, 10, 14, 15, 35, 52, 513, DateTimeKind.Local).AddTicks(197));
+        }
+    }
+}
