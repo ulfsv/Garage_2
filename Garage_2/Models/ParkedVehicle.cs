54097cd8320dd322e7949225fc57afc07492da33
Fatma Alhouthi <fa222nx@student.lnu.se>
2020-10-14 13:19:09 +0200

    add parked vehicle and vehicle classes

---
+++ Garage_2/Models/ParkedVehicle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Garage_2.Models {
+    public class ParkedVehicle {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public VehicleTypeEnum VehicleType { get; set; }
+        [Required]
+        public string RegisterNumber { get; set; }
+        [Required]
+        public string Color { get; set; }
+        [Required]
+        public string Model { get; set; }
+        [Required]
+        public string Brand { get; set; }
+        [Required]
+        public int WheelsNumber { get; set; }
+        [Required]
+        public DateTime ChechInDateTime { get; set; }
+        [Required]
+        public DateTime CheckOutDateTime { get; set; }
+    }
+
+}
