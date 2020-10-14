04805c6a43154c281b4fba73ffd7df2942a721b7
stelstel <sthlm_32@hotmail.com>
2020-10-14 15:08:50 +0200

    Seeding

--- Garage_2/Controllers/ParkedVehiclesController.cs
+++ Garage_2/Controllers/ParkedVehiclesController.cs
@@ -54,7 +54,7 @@ namespace Garage_2.Controllers
         // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,VehicleType,RegisterNumber,Color,Model,Brand,WheelsNumber,ChechInDateTime,CheckOutDateTime")] ParkedVehicle parkedVehicle)
+        public async Task<IActionResult> Create([Bind("Id,VehicleType,RegisterNumber,Color,Model,Brand,WheelsNumber,ParkedDateTime,CheckOutDateTime")] ParkedVehicle parkedVehicle)
         {
             if (ModelState.IsValid)
             {
@@ -86,7 +86,7 @@ namespace Garage_2.Controllers
         // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,VehicleType,RegisterNumber,Color,Model,Brand,WheelsNumber,ChechInDateTime,CheckOutDateTime")] ParkedVehicle parkedVehicle)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,VehicleType,RegisterNumber,Color,Model,Brand,WheelsNumber,ParkedDateTime,CheckOutDateTime")] ParkedVehicle parkedVehicle)
         {
             if (id != parkedVehicle.Id)
             {
