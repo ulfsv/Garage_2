e881b6a375eeb3b5cb3d7790acf75595a9db64d0
Mattias Wallerman <wallermanmattias@yahoo.se>
2020-10-14 13:50:40 +0200

    parked controllers

---
+++ Garage_2/Controllers/ParkedVehiclesController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Garage_2.Data;
+using Garage_2.Models;
+
+namespace Garage_2.Controllers
+{
+    public class ParkedVehiclesController : Controller
+    {
+        private readonly Garage_2Context _context;
+
+        public ParkedVehiclesController(Garage_2Context context)
+        {
+            _context = context;
+        }
+
+        // GET: ParkedVehicles
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.ParkedVehicle.ToListAsync());
+        }
+
+        // GET: ParkedVehicles/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var parkedVehicle = await _context.ParkedVehicle
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (parkedVehicle == null)
+            {
+                return NotFound();
+            }
+
+            return View(parkedVehicle);
+        }
+
+        // GET: ParkedVehicles/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ParkedVehicles/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,VehicleType,RegisterNumber,Color,Model,Brand,WheelsNumber,ChechInDateTime,CheckOutDateTime")] ParkedVehicle parkedVehicle)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(parkedVehicle);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(parkedVehicle);
+        }
+
+        // GET: ParkedVehicles/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var parkedVehicle = await _context.ParkedVehicle.FindAsync(id);
+            if (parkedVehicle == null)
+            {
+                return NotFound();
+            }
+            return View(parkedVehicle);
+        }
+
+        // POST: ParkedVehicles/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,VehicleType,RegisterNumber,Color,Model,Brand,WheelsNumber,ChechInDateTime,CheckOutDateTime")] ParkedVehicle parkedVehicle)
+        {
+            if (id != parkedVehicle.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(parkedVehicle);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ParkedVehicleExists(parkedVehicle.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(parkedVehicle);
+        }
+
+        // GET: ParkedVehicles/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var parkedVehicle = await _context.ParkedVehicle
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (parkedVehicle == null)
+            {
+                return NotFound();
+            }
+
+            return View(parkedVehicle);
+        }
+
+        // POST: ParkedVehicles/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var parkedVehicle = await _context.ParkedVehicle.FindAsync(id);
+            _context.ParkedVehicle.Remove(parkedVehicle);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ParkedVehicleExists(int id)
+        {
+            return _context.ParkedVehicle.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Garage_2/Data/Garage_2Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Garage_2.Models;
+
+namespace Garage_2.Data
+{
+    public class Garage_2Context : DbContext
+    {
+        public Garage_2Context (DbContextOptions<Garage_2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Garage_2.Models.ParkedVehicle> ParkedVehicle { get; set; }
+    }
+}
