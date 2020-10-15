04a97ed5c88fb5ced12135a63cdfc95c539dc122
stelstel <sthlm_32@hotmail.com>
2020-10-15 23:24:35 +0200

    Small changes to Receipt

--- Garage_2/Models/ReceiptViewModel/Receipt.cs
+++ Garage_2/Models/ReceiptViewModel/Receipt.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Garage_2.Models.ReceiptViewModel {
     public class Receipt {
+        [Display(Name = "Price per hour")]
+        [DataType(DataType.Currency)]
         public int PricePerHour { get; set; }
+
+        [Display(Name = "Registration number")]
         public string RegNr { get; set; }
+
+        [Display(Name = "Vehicle parked at time")]
         public DateTime ParkingTime { get; set; }
         
 
