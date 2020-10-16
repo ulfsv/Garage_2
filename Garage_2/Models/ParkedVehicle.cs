a97c74caa0e0da74f2bbd33662da722d1b60bbbe
stelstel <sthlm_32@hotmail.com>
2020-10-16 15:39:50 +0200

    Uppercase Reg No

--- Garage_2/Models/ParkedVehicle.cs
+++ Garage_2/Models/ParkedVehicle.cs
@@ -19,7 +19,14 @@ namespace Garage_2.Models {
         [Display(Name = "Register No")]
         [RegularExpression(@"^[a-zA-Z]{3}\d{3}$", ErrorMessage = "Wrong format should be ABC123.")]
 
-        public string RegisterNumber { get; set; }
+        private string registerNumber;
+
+        public string RegisterNumber
+        {
+            get { return registerNumber; }
+            set { registerNumber = value.ToUpper(); }
+        }
+
         [Required]
         public string Color { get; set; }
         [Required]
