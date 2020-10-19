15c9495812e91ef30a509feb72456b980a502f64
stelstel <sthlm_32@hotmail.com>
2020-10-19 17:47:38 +0200

    JS feedback

--- Garage_2/wwwroot/js/site.js
+++ Garage_2/wwwroot/js/site.js
@@ -1,7 +1,7 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-if (sessionStorage.getItem("msgToUser")!== null && sessionStorage.msgShown != true) {
+if (sessionStorage.getItem("msgToUser")!== null && sessionStorage.msgShown !== "true") {
     $("#alert-user").text(sessionStorage.msgToUser);
     sessionStorage.msgShown = true;
     $("#alert-user").show();
@@ -21,6 +21,11 @@ if (document.querySelector("#btn-edit-vehicle")) {
     btnEditVehicle.addEventListener("click", onBtnEditVehicleClick);
 }
 
+if (document.querySelector("#link-fetch-vehicle")) {
+    var linkFetchVehicle = document.querySelector("#link-fetch-vehicle");
+    linkFetchVehicle.addEventListener("click", onLinkFetchVehicleClick);
+}
+
 function onBtnParkVehicleClick() {
     sessionStorage.msgToUser = "Vehicle was parked";
     sessionStorage.msgShown = false;
@@ -28,5 +33,10 @@ function onBtnParkVehicleClick() {
 
 function onBtnEditVehicleClick() {
     sessionStorage.msgToUser = "Vehicle was edited";
+    sessionStorage.msgShown = false;
+}
+
+function onLinkFetchVehicleClick() {
+    sessionStorage.msgToUser = "Vehicle was fetched";
     sessionStorage.msgShown = false;
 }
