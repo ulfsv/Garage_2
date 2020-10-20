38b6edf2c12b1d06bbbf90b0e96e2ef7dfda15cc
stelstel <sthlm_32@hotmail.com>
2020-10-20 17:03:19 +0200

    Better CalculateTime

--- Garage_2/Controllers/ParkedVehiclesController.cs
+++ Garage_2/Controllers/ParkedVehiclesController.cs
@@ -206,7 +206,7 @@ namespace Garage_2.Controllers
     
         public int CalculateTime( DateTime TimeParked)
         {
-            int totalTime = (((DateTime.Now.Day-TimeParked.Day)*24)+ DateTime.Now.Hour - TimeParked.Hour);
+            int totalTime = (int)Math.Ceiling(((((DateTime.Now.Day - TimeParked.Day) * 24 * 60) + (DateTime.Now.Hour - TimeParked.Hour) * 60 + DateTime.Now.Minute - TimeParked.Minute)) / 60.0);
             return totalTime;
            
         }
